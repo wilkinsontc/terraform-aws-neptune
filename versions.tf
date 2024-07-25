11094fb04fddbb6975f819af6ed01712a172e0df
Tim Wilkinson <t.wilkinson@churchofjesuschrist.org>
2024-07-25 16:46:06 -0600

    Updating the version provider version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=2.0,<4.0"
+      version = ">=2.0,<6.0"
     }
   }
   required_version = ">= 0.13,< 2.0"
